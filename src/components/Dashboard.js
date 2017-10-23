f5dd6da5b6d4bb986a1bbde3c6470c4b400a39aa
Norik Davtian <norik@norik.io>
2017-10-22 20:11:35 -0700

    sidebar stylized
    Signed-off-by: Norik Davtian <norik@norik.io>

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import { Segment } from 'semantic-ui-react';
 import TopNav from './TopNav';
+import Sidebar from './Sidebar';
 import './Dashboard.css';
 
 const Dashboard = () => (
   <div id="dashboard">
-    <TopNav />
-    <Segment basic>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis,
-        laudantium, quod. Accusantium, ad amet animi asperiores blanditiis
-        delectus doloremque, eos modi odio officiis perferendis quae quam
-        repellat, repellendus ut? Natus?</p>
-    </Segment>
+    <div id="main" className="toggled">
+      <TopNav />
+      <Segment basic id="content">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis,
+          laudantium, quod. Accusantium, ad amet animi asperiores blanditiis
+          delectus doloremque, eos modi odio officiis perferendis quae quam
+          repellat, repellendus ut? Natus?</p>
+      </Segment>
+    </div>
+    <Sidebar />
   </div>
 );
 
