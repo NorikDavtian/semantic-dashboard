092cfa01b6611a9278c8e93418d408d7f5742d31
Norik Davtian <norik@norik.io>
2019-08-04 15:15:30 -0700

    replace react router dom with reach router

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,15 +1,25 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Router } from '@reach/router';
+import NotFound from '../components/NotFound';
 import Dashboard from './Dashboard';
 import LoginForm from '../components/Login';
+import Inbox from '../components/Inbox';
+import inboxService from '../services/inbox';
+import Email from '../components/Email';
+import emailService from '../services/email';
 
-const App = ({ store }) => (
-  <div id="app">
-    <Switch>
-      <Route exact path={'/login'} component={() => <LoginForm store={store} />} />
-      <Route path={'/'} component={() => <Dashboard store={store} />} />
-    </Switch>
-  </div>
-);
+const App = ({ store }) => {
+  console.log(store);
+  return (
+    <Router id="app">
+      <LoginForm path="/login" />
+      <Dashboard path="/">
+        <NotFound default />
+        <Inbox exact path="/" service={inboxService(store)} />
+        <Email path="email/:emailId" service={emailService(store)} />
+      </Dashboard>
+    </Router>
+  );
+};
 
 export default App;
--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -1,81 +1,10 @@
-import React, { useEffect, useState } from 'react';
-import { Route, Switch } from 'react-router-dom';
+import React, { useState } from 'react';
 import TopNav from '../components/TopNav';
 import Sidebar from '../components/Sidebar';
-import Inbox from '../components/Inbox';
 import './Dashboard.css';
-import Email from '../components/Email';
 
-
-const Dashboard = ({ store }) => {
-  // async componentWillMount() {
-  //   try {
-  //     storedState = await store.get(Dashboard.name);
-  //     if (storedState) {
-  //       this.setState(storedState);
-  //     }
-  //   } catch (_err) {
-  //     await store.put(this.state);
-  //   }
-  //   store.changes({
-  //     since: 'now',
-  //     live: true,
-  //     include_docs: true
-  //   }).on('change', (change) => {
-  //     if (change.id === 'Sidebar') {
-  //       const updatedState = { isSidebarToggledOpen: change.doc.isToggledOpen };
-  //       this.updateState(store, updatedState);
-  //     }
-  //   }).on('error', error => console.error(error));
-  // }
-  //
-  // updateState = async (store, updatedState) => {
-  //   this.setState(updatedState);
-  //   const doc = await store.get(Dashboard.name);
-  //   store.put(Object.assign(doc, updatedState));
-  // };
-
-  const [emails, setEmails] = useState([]);
-  const [email, setEmail] = useState({});
+const Dashboard = ({ children }) => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(true);
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(false);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      setError(false);
-      setIsLoading(true);
-      try {
-        const data = await store.API.inbox.getEmails();
-        setEmails(data);
-      } catch (_err) {
-        setError('Cannot fetch inbox emails');
-      }
-      setIsLoading(false);
-    };
-
-    fetchData();
-  }, [store]);
-
-  // No unused vars for now
-  console.log('setEmail', setEmail);
-  console.log('error', error);
-  console.log('isLoading', isLoading);
-
-  const emailHandler = async (id) => {
-    try {
-      const data = await store.API.email.getEmail(id);
-      console.log('email', data);
-      store.history.push({
-        pathname: `/email/${id}`,
-        state: { fromInbox: true }
-      });
-      this.render();
-    } catch (_err) {
-      console.error(_err);
-      setError(`Cannot fetch email ${id}`);
-    }
-  };
 
   const toggleSidebar = () => setIsSidebarOpen(!isSidebarOpen);
 
@@ -83,10 +12,7 @@ const Dashboard = ({ store }) => {
     <div id="dashboard">
       <div id="main" className={isSidebarOpen ? 'open' : ''}>
         <TopNav toggleSidebar={toggleSidebar} />
-        <Switch>
-          <Route exact path={'/'} component={() => <Inbox emailHandler={emailHandler} emails={emails} />} />
-          <Route path={'/email/:id'} component={() => <Email email={email} />} />
-        </Switch>
+        <div>{children}</div>
       </div>
       <Sidebar isToggledOpen={isSidebarOpen} />
     </div>
